4171b462c84eee4c96323a7c7fa42bdb2323680b
Jonatas Alves <jonatas.a2012@hotmail.com>
2023-07-16 23:52:19 -0300

    changed url fetch

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,7 @@
 export async function fetchApi(
   input: RequestInfo | URL, 
   init?: RequestInit | undefined) {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${input}`, init);
+  const response = await fetch(`https://parallelum.com.br/fipe/api/v1${input}`, init);
   const dataJson = await response.json();
   return dataJson;
 }
