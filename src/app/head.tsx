15718de300f6434b872094aceb9b6a2b8a9c36fe
Jonatas Alves <jonatas.a2012@hotmail.com>
2023-07-17 00:48:09 -0300

    remove metadata and added head

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>mobiauto - teste</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="mobiauto - teste" />
+    </>
+  );
+}
